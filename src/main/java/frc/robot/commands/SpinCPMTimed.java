b4ab946248d6f0114f1e3d831da03a250eb3de51
Pacheco <pachecol@centinela.k12.ca.us>
2020-02-26 19:12:36 -0800

    Update SpinCPMTimed.java

--- src/main/java/frc/robot/commands/SpinCPMTimed.java
+++ src/main/java/frc/robot/commands/SpinCPMTimed.java
@@ -7,12 +7,15 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ControlPanelManipulator;
 
 public class SpinCPMTimed extends CommandBase {
   /**
    * Creates a new SpinCPMTimed.
    */
+  ControlPanelManipulator cpm = ControlPanelManipulator.GetInstance();
   public SpinCPMTimed() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -25,6 +28,8 @@ public class SpinCPMTimed extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+  cpm.SpinControlPanel();
+  Timer.delay(1);
   }
 
   // Called once the command ends or is interrupted.
@@ -35,6 +40,7 @@ public class SpinCPMTimed extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    cpm.StopSpinControlPanel();
+    return true;
   }
 }
